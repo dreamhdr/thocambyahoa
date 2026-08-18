0e1c79e55b8e5a6f6d11b756e28b1ea7ed71c35d
Aura <dreamhdr@gmail.com>
2026-08-18 12:46:59 +0700

    Force rebuild to clear cache

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Force rebuild - timestamp: 2026-08-18 05:45
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
